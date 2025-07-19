be6051beb5cf01013dc2ebba04f1284777e0f2f2
Harsh Sawant <hrsh.swnt@usask.ca>
2025-07-19 00:07:29 -0600

    added json support for both

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Check environment variable for base URL
+const base = process.env.VITE_TARGET === 'gh-pages' ? '/sudoku/' : './';
 
 export default defineConfig({
-  base: './',
+  base,
   plugins: [react()],
-})
+});
